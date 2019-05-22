501509f05f5e5e733071e99e269d531da3fc7a03
ewelinaMakowska <ewelina.makowska0@gmail.com>
2019-05-22 19:41:02 +0200

    Updates

---
+++ precache-manifest.2c83cf1924cb05621bda289e6988ff62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b231bbb7132e786c00ca606aac7277",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5d26718a790f12080d10",
+    "url": "/weather-app/static/css/main.39d5cd20.chunk.css"
+  },
+  {
+    "revision": "9049407d14ebb8ff8711",
+    "url": "/weather-app/static/js/2.ec3f22b6.chunk.js"
+  },
+  {
+    "revision": "5d26718a790f12080d10",
+    "url": "/weather-app/static/js/main.549712e2.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  },
+  {
+    "revision": "c0ee1821b6e883b3e508e675ec2d0e58",
+    "url": "/weather-app/static/media/background-1.c0ee1821.jpg"
+  }
+]);
